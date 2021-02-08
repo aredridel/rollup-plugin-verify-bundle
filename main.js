3f27e3c19229d10f812fb7a6a67e76fa4bdb7aa9
Aria Stewart <aredridel@dinhe.net>
2021-02-08 11:42:47 -0500

    Verify imports are available both structurally and by type

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ export default function verify(opts) {
 
     return {
         name: 'verify-build', // this name will show up in warnings and errors
-        generateBundle(options, bundle, isWrite) {
+        generateBundle(_options, bundle) {
             if (opts.noExternals) {
                 const deps = new Set(Object.keys(bundle));
                 const unknown = new Set();
                 for (const output of Object.values(bundle)) {
                     if (bundle.type != 'chunk') continue;
+                    if (!output.imports) continue;
                     for (const dep of output.imports) {
                         if (!deps.has(dep)) unknown.add(dep);
                     }
