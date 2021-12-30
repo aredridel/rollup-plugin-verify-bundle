e3e4ab1f01da8ef0937e81c5781a2e62911fc994
Aria Stewart <aredridel@dinhe.net>
2021-12-30 16:50:34 -0500

    Convert to esm

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-require = require("esm")(module)
-module.exports = require("./main.js")
+export function verify(opts) {
+
+    return {
+        name: 'verify-build', // this name will show up in warnings and errors
+        /**
+         * @param {any} _options
+         * @param {import('rollup').OutputBundle} bundle
+         */
+        generateBundle(_options, bundle) {
+            if (opts.noExternals) {
+                const deps = new Set(Object.keys(bundle));
+                const unknown = new Set();
+                for (const output of Object.values(bundle)) {
+                    if (bundle.type != 'chunk') continue;
+                    if (!output.imports) continue;
+                    for (const dep of output.imports) {
+                        if (!deps.has(dep)) unknown.add(dep);
+                    }
+                }
+
+                if (unknown.size) {
+                    throw new Error(`Found unknown external reference(s) ${ [...unknown].map(e => `"${e}"`).join(", ") }`);
+                }
+
+            }
+        }
+    }
+};
+
+export default verify;
